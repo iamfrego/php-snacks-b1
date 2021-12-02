1529f54184fddf6ddb2b7f30327bbf02ebb59478
iamfrego <mate_freg@tiscali.it>
2021-12-02 15:40:27 +0100

    Snack 4 complete

---
+++ snack 4/intex.php
@@ -0,0 +1,10 @@
+<?php
+$random_array = [];
+while (count($random_array) < 15) {
+    $random_number = rand(1, 100);
+    if (!in_array($random_number, $random_array)) {
+        $random_array[] = $random_number;
+    }
+}
+
+?>
