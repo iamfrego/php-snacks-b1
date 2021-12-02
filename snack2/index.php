fe46f42e2c561fc07a803fe3df91dd9badf3a906
iamfrego <mate_freg@tiscali.it>
2021-12-02 12:27:23 +0100

    Snack 2 complete

--- snack2/index.php
+++ snack2/index.php
@@ -7,7 +7,45 @@ IF
 strlen
 strpos
 is_numeric
-p
 
 -->
 
+
+<?php
+
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+$eta = $_GET['eta'];
+
+if (strlen($nome) > 3 && is_numeric($eta)) {
+    if (strpos($email, '@') && strpos($email, '.')) {
+        $result = 'Accesso riuscito';
+    } else {
+        $result = 'Accesso negato';
+    }
+} else {
+    $result = 'Accesso negato';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks2</title>
+</head>
+<body>
+    <form action="./index.php" method="get">
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" id="nome">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email">
+        <label for="eta">Età</label>
+        <input type="text" name="eta" id="età">
+        <input type="submit" value="Login">
+    </form>
+
+    <h1>Risultato: <?php echo $result; ?></h1>
+</body>
+</html>
