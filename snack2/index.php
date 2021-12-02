fe3083391e5483f14086af120b25985ce2940e3c
iamfrego <mate_freg@tiscali.it>
2021-12-02 12:10:37 +0100

    Snack 2 tools

---
+++ snack2/index.php
@@ -0,0 +1,13 @@
+<!-- Passare come parametri GET name, mail e age e verificare che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
+
+GET 
+FORM
+IF
+&&
+strlen
+strpos
+is_numeric
+p
+
+-->
+
