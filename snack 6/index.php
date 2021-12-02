82bb09b27c288c228bb14adb275db0c1e2d25e8e
iamfrego <mate_freg@tiscali.it>
2021-12-02 17:15:45 +0100

    Snack 6 fix

--- snack 6/index.php
+++ snack 6/index.php
@@ -1,67 +1,78 @@
 <?php
-    $db = [
-        'teachers' => [
-            [
-                'name' => 'Michele',
-                'lastname' => 'Papagni'
-            ],
-            [
-                'name' => 'Fabio',
-                'lastname' => 'Forghieri'
-            ]
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
         ],
-        'pm' => [
-            [
-                'name' => 'Roberto',
-                'lastname' => 'Marazzini'
-            ],
-            [
-                'name' => 'Federico',
-                'lastname' => 'Pellegrini'
-            ]
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
         ]
-    ];
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>snack 6</title>
     <style>
-        .d-flex{
+        .d-flex {
             display: flex;
         }
-        .teachers, .pm{
+
+        .teachers,
+        .pm {
             margin: 1rem;
-            padding:3rem;
+            padding: 3rem;
             color: dark;
         }
-        .teachers{
+
+        .teachers {
             background-color: gray;
         }
-        .pm{
+
+        .pm {
             background-color: #26ff00;
         }
     </style>
 </head>
+
 <body>
     <div class="d-flex">
         <div class="teachers">
             <ul>
-                <?php for ($i=0; $i < count($db['teachers']); $i++) { ?>
-                    <li><?= $db['teachers'][$i]['name'] . ' ' . $db['teachers'][$i]['lastname'] ?></li>
+                <?php for ($i = 0; $i < count($db['teachers']); $i++) { ?>
+                    <li>
+                        <?= $db['teachers'][$i]['name'] . ' ' . $db['teachers'][$i]['lastname'] ?>
+                    </li>
                 <?php }; ?>
             </ul>
         </div>
         <div class="pm">
             <ul>
-                <?php for ($i=0; $i < count($db['pm']); $i++) { ?>
-                    <li><?= $db['pm'][$i]['name'] . ' ' . $db['pm'][$i]['lastname'] ?></li>
+                <?php for ($i = 0; $i < count($db['pm']); $i++) { ?>
+                    <li>
+                        <?= $db['pm'][$i]['name'] . ' ' . $db['pm'][$i]['lastname'] ?>
+                    </li>
                 <?php }; ?>
             </ul>
         </div>
     </div>
 </body>
+
 </html>
